eccafddf249afdd736ba33e55218805cfb0bdfe3
ItzYanick <me@yanick.gay>
2026-08-13 09:34:10 +0200

    omit unavailable udisks Settings panel

--- pkgs/gnome-control-center/prepare.sh
+++ pkgs/gnome-control-center/prepare.sh
@@ -7,3 +7,8 @@ set -eu
 # live image, so omit this one panel from both compilation and registration.
 sed -i "/^[[:space:]]*'printers',[[:space:]]*$/d" "$SRC_PATH/panels/meson.build"
 sed -i '/^[[:space:]]*"printers",[[:space:]]*$/d' "$SRC_PATH/shell/cc-panel-list.c"
+
+# The System panel requires udisks2. Its storage stack is not packaged yet;
+# keep the rest of Settings usable while that independent closure is finished.
+sed -i "/^[[:space:]]*'system',[[:space:]]*$/d" "$SRC_PATH/panels/meson.build"
+sed -i '/^[[:space:]]*"system",[[:space:]]*$/d' "$SRC_PATH/shell/cc-panel-list.c"
