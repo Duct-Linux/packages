3cddfaa0b55572d953061e8dbaf3846bac028d9f
ItzYanick <me@yanick.gay>
2026-08-13 09:46:32 +0200

    remove omitted Settings panels from loader

--- pkgs/gnome-control-center/prepare.sh
+++ pkgs/gnome-control-center/prepare.sh
@@ -7,8 +7,10 @@ set -eu
 # live image, so omit this one panel from both compilation and registration.
 sed -i "/^[[:space:]]*'printers',[[:space:]]*$/d" "$SRC_PATH/panels/meson.build"
 sed -i '/^[[:space:]]*"printers",[[:space:]]*$/d' "$SRC_PATH/shell/cc-panel-list.c"
+sed -i '/cc_printers_panel_get_type/d' "$SRC_PATH/shell/cc-panel-loader.c"
 
 # The System panel requires udisks2. Its storage stack is not packaged yet;
 # keep the rest of Settings usable while that independent closure is finished.
 sed -i "/^[[:space:]]*'system',[[:space:]]*$/d" "$SRC_PATH/panels/meson.build"
 sed -i '/^[[:space:]]*"system",[[:space:]]*$/d' "$SRC_PATH/shell/cc-panel-list.c"
+sed -i '/cc_system_panel_get_type/d' "$SRC_PATH/shell/cc-panel-loader.c"
